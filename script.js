347e783d4eca5507d52f8afdd00f6d70c5e20d83
Grant Coburn <140763612+coburnGrant@users.noreply.github.com>
2025-06-30 20:59:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // Event listener for dom content loaded
+const apiUrl = 'https://verdant-bolder-reward.glitch.me/api/songs'; // 'http://localhost:3000/api/songs'
 
 document.addEventListener("DOMContentLoaded", async () => {
-    const response = await fetch('http://localhost:3000/api/songs');
+    const response = await fetch(apiUrl);
 
     const songs = await response.json();
 
